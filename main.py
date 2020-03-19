2681511cc803e4e4d63b594e7f3567ba5c444849
nonk123 <Sergeyii321@gmail.com>
2020-03-19 13:35:34 +0300

    Tidy up main.py

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
 
 from fuse import FUSE
-
 from mpd_fs import MpdFilesystem
-
-import util
-
-def main():
-    fs = MpdFilesystem()
-    FUSE(fs, util.ensure_dir_exists("Music/"), foreground=True)
+from util import ensure_dir_exists
 
 if __name__ == "__main__":
-    main()
+    FUSE(MpdFilesystem(), ensure_dir_exists("Music/"), foreground=True)
